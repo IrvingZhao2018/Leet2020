e2c3ed8f695ec9c639a76c024424e3ee0d20da21
irving <irvingzqy@gmail.com>
2020-08-25 00:49:52 -0400

    404 Sum of left leaves

---
+++ src/Tree/leet404SumOfLeft/Solution.java
@@ -0,0 +1,29 @@
+package Tree.leet404SumOfLeft;
+
+import Tree.TreeNode;
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                cur = cur.right;
+            } else {
+                TreeNode predecessor = cur.left;
+                if(predecessor.left == null && predecessor.right == null)
+                    sum += predecessor.val;
+                while(predecessor.right != null && predecessor.right != cur)
+                    predecessor = predecessor.right;
+                if(predecessor.right == null){
+                    predecessor.right = cur;
+                    cur = cur.left;
+                } else {
+                    predecessor.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return sum;
+    }
+}
