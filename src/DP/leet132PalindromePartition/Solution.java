226e44e10b3309f0217222a73837478b80e431ac
irving <irvingzqy@gmail.com>
2020-09-10 19:39:39 -0400

    DP

--- src/DP/leet132PalindromePartition/Solution.java
+++ src/DP/leet132PalindromePartition/Solution.java
@@ -27,6 +27,39 @@ class Solution {
         return min;
     }
 
+    public int minCutDP2D(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] cut = new int[n];
+        for(int i = 0; i < n; i++){
+            int min = n;
+            for(int j = 0; j <= i; j++){
+                if(s.charAt(i) == s.charAt(j) && (i - j <= 2 || dp[j + 1][i - 1])){
+                    dp[j][i] = true;
+                    min = j == 0 ? 0 : Math.min(min, 1 + cut[j - 1]);
+                }
+            }
+            cut[i] = min;
+        }
+        return cut[n - 1];
+    }
+
+    public int minCutDP1D(String s) {
+        int n = s.length();
+        int[] cut = new int[n + 1];
+        for(int i = 0; i <= n; i++)
+            cut[i] = i - 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; i - j >= 0 && i + j < n && s.charAt(i - j) == s.charAt(i + j); j++){
+                cut[i + j + 1] = Math.min(cut[i + j + 1], 1 + cut[i - j]);
+            }
+            for(int j = 1; i - j + 1 >= 0 && i + j < n && s.charAt(i - j + 1) == s.charAt(i + j); j++){
+                cut[i + j + 1] = Math.min(cut[i + j + 1], 1 + cut[i - j + 1]);
+            }
+        }
+        return cut[n];
+    }
+
 
 
 }
